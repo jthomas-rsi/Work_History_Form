f69afa14f1c407f89c7e5a5deb5bd4bb3c289e43
jthomas-rsi <112565472+jthomas-rsi@users.noreply.github.com>
2023-01-30 14:58:21 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./styles.css";
+import WorkHistory from "../src/components/WorkHistory";
+
+export default function App() {
+  return <WorkHistory />;
+}
---
+++ src/components/WorkHistory.tsx
@@ -0,0 +1,31 @@
+/**
+  TODO: Work History Form
+   (1): create a work history form 
+   (2): include basic information gathering section
+    -> Name
+    -> age
+    -> address
+    -> phone#
+    -> email
+    -> country
+   (3): include nested sections for specific information of form
+    -> work history  
+      --> Company name
+      --> Job title 
+      --> Job description 
+      --> Start Date
+      --> End Date
+      --> Supervisor
+      --> Supervisor Contact information
+   (4): collect all form information    
+   (5): submit form inormation 
+   (6): display submitted information conditionally 
+   (7): reset app to Work History after data displayed onSubmit   
+
+ */
+
+const WorkHistory = () => {
+  return <>{"future home of the Work History Form"}</>;
+};
+
+export default WorkHistory;
