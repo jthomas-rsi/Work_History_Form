7a58f9e3335dc5ae928065b3563624b906d06f91
James F. Thomas <jthomas@rsimail.com>
2023-01-30 15:13:33 -0600

    npm install first / commit from local machine

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
-
+if (!rootElement) throw new Error("Failed to find root element");
+const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
