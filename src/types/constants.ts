83b5d37d851c3fc513d6f84cf1dde943d035f496
James F. Thomas <jthomas@rsimail.com>
2023-02-20 16:17:24 -0600

    cretad constants object for text display

---
+++ src/types/constants.ts
@@ -0,0 +1,28 @@
+import { DisplayText } from "./types";
+
+export const displayText: DisplayText = {
+    applicantValues:{
+        formTitle: `Submission Type:`,
+        formDate: `Submission Date:`,
+        firstName: `First Name:`,
+        lastName: `Last Name:`,
+        gender: `Gender:`,
+        age: `Age:`,
+        dateOfBirth: `Date Of Birth:`,
+        address: `Address:`,
+        cityStateZip: `City/State/Zip:`,
+        country: `Country:`,
+        phoneNumber: `Phone#:`,
+        email: `Email:`,
+    },
+    historyValues:{
+        companyName: "Company Name:",
+        positionTitle: "Position Title:",
+        startDate: "Start Date:",
+        endDate:"End Date:",
+        positionDescription: "Position Description",
+        supervisorName: `Supervisor's Name:`,
+        supervisorContact: `Supervisor's Contact:`, 
+        contactApproval: `Permission to Contact:`,
+    }
+}
